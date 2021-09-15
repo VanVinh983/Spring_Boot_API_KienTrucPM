0173e4c4ae02a49ecc804224cdc3124c36a92a2b
VanVinh983 <doanvinh983@gmail.com>
2021-09-15 11:44:16 +0700

    commit branch crud_basic

--- src/main/java/com/example/Spring_Boot_API/rest/BookRestController.java
+++ src/main/java/com/example/Spring_Boot_API/rest/BookRestController.java
@@ -1,12 +1,55 @@
 package com.example.Spring_Boot_API.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Spring_Boot_API.entity.Book;
 import com.example.Spring_Boot_API.service.BookService;
 
 @RestController
+@RequestMapping("/api")
 public class BookRestController {
+	
 	@Autowired
 	private BookService bookService;
+	
+	@GetMapping("/book")
+	public List<Book> findAllBook() {
+		return bookService.getAllBook();
+	}
+	
+	@PostMapping("/book")
+	public Book addBook(@RequestBody Book book) {
+		bookService.addBook(book);
+		return book;
+	}
+	@DeleteMapping("/book/{id}")
+	public String deleteBook(@PathVariable("id") Long id) {
+		Book book = bookService.getBookById(id);
+		if (book == null) {
+			throw new RuntimeException("book id not found - "+id);
+		}else {
+			bookService.deleteBook(book);
+			return "delete sucessfull id "+id;
+		}
+	}
+	@PutMapping("/book/{id}")
+	public Book update(@RequestBody Book book, @PathVariable("id") Long id) {
+		if (book == null) {
+			throw new RuntimeException("book null");
+		}else {
+			book.setId(id);
+			bookService.updateBook(book);
+			return book;
+		}
+	}
 }
--- src/main/java/com/example/Spring_Boot_API/serviceImpl/BookServiceImpl.java
+++ src/main/java/com/example/Spring_Boot_API/serviceImpl/BookServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.Spring_Boot_API.serviceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.Spring_Boot_API.entity.Book;
 import com.example.Spring_Boot_API.repository.BookRepsitory;
 import com.example.Spring_Boot_API.service.BookService;
 
@@ -10,4 +13,33 @@ import com.example.Spring_Boot_API.service.BookService;
 public class BookServiceImpl implements BookService{
 	@Autowired
 	private BookRepsitory bookRepsitory;
+
+	@Override
+	public void addBook(Book book) {
+		bookRepsitory.save(book);
+	}
+
+	@Override
+	public void deleteBook(Book book) {
+		// TODO Auto-generated method stub
+		bookRepsitory.delete(book);
+	}
+
+	@Override
+	public void updateBook(Book book) {
+		// TODO Auto-generated method stub
+		bookRepsitory.save(book);
+	}
+
+	@Override
+	public List<Book> getAllBook() {
+		// TODO Auto-generated method stub
+		return bookRepsitory.findAll();
+	}
+
+	@Override
+	public Book getBookById(Long id) {
+		// TODO Auto-generated method stub
+		return bookRepsitory.getById(id);
+	}
 }
